1813de5db8b3bcfdc5c5786e12a09d8eda8939c3
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 19:55:47 +0000

    Exibe uma lista de turmas mock no `ListaTurmasComponent`.

--- src/app/components/lista-turmas/lista-turmas.component.ts
+++ src/app/components/lista-turmas/lista-turmas.component.ts
@@ -1,11 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Turma } from 'src/app/models/turma';
+import { Disciplina } from 'src/app/models/disciplina';
+
 @Component({
   selector: 'app-lista-turmas',
   templateUrl: './lista-turmas.component.html',
   styleUrls: ['./lista-turmas.component.css'],
 })
 export class ListaTurmasComponent implements OnInit {
+
+  public turmas: Turma[] = [
+    new Turma(
+      new Disciplina(
+        'WEB01',
+        'Fundamentos do Desenvolvimento Web',
+      ),
+      2021,
+      1,
+      [
+      ],
+    ),
+    new Turma(
+      new Disciplina(
+        'WEB02',
+        'Introdução aos Frameworks',
+      ),
+      2021,
+      2,
+      [
+      ],
+    ),
+  ];
 
   constructor(
   ) {
