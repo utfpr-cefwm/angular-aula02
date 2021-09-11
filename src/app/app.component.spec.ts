23b4968b717d9e1d6ac8b91b96b1657404c0abae
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-09-11 19:54:37 +0000

    Cria layout de menu e de área de visualização de turma.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,7 +1,18 @@
+import { Component } from '@angular/core';
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { AppComponent } from './app.component';
+
+@Component({
+  selector: 'app-lista-turmas',
+  template: `
+  `,
+  styles: [`
+  `],
+})
+export class ListaTurmasComponentMock {
+}
 
 describe('AppComponent', () => {
 
@@ -12,6 +23,7 @@ describe('AppComponent', () => {
       ],
       declarations: [
         AppComponent,
+        ListaTurmasComponentMock,
       ],
     }).compileComponents();
   });
