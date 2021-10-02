96da69bbe2d393d68db3621de035fb50967f5754
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-02 12:24:37 +0000

    Cria o `TurmaComponent`.
    - Comando:
      ng g c components/turma

---
+++ src/app/components/turma/turma.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-turma',
+  templateUrl: './turma.component.html',
+  styleUrls: ['./turma.component.css'],
+})
+export class TurmaComponent implements OnInit {
+
+  constructor(
+  ) {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
