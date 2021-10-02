afd8f7e58824162a2ca947d221db6d9e109d4f23
Rui Pimentel Leite <ruipimentelleite@gmail.com>
2021-10-02 18:26:57 +0000

    Configura a API para habilitar nosso front end para ler a Response HTTP.

--- api.js
+++ api.js
@@ -1,11 +1,15 @@
 const express = require('express');
 
+const cors = require('cors');
+
 const turmasRouter = require('./api/turmas');
 
 const API_PORT = 3001;
 const API_PATH = 'api';
 
 const app = express();
+
+app.use(cors());
 
 app.use(`/${API_PATH}/turmas`, turmasRouter);
 
